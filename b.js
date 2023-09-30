b1b69ed8bfd342fe82b3ca608649a9c53b4d9556
aljaarir <133610949+aljaarir@users.noreply.github.com>
2023-09-30 19:47:18 -0400

    b.js

--- b.js
+++ b.js
@@ -7,7 +7,8 @@ document.getElementById('student-form').addEventListener('submit', function (e) 
     })
         .then(response => response.text())
         .then(data => {
-            document.getElementById('results').style.display = 'block';
+            document.getElementById('info-container').style.display = 'none'; // Hide the form
+            document.getElementById('results').style.display = 'block'; // Show the results
             document.querySelector('#results table').innerHTML = data;
         });
 });
