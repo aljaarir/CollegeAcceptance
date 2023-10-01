1fa73b3feed0829c4b00cdb362a17b4e3d15d41a
aljaarir <133610949+aljaarir@users.noreply.github.com>
2023-09-30 21:08:33 -0400

    b.js

--- b.js
+++ b.js
@@ -1,14 +1,17 @@
 document.getElementById('student-form').addEventListener('submit', function (e) {
     e.preventDefault();
+    console.log('Form submitted'); // Add this line for debugging
     const formData = new FormData(this);
-    fetch('/results.py', { // Update the URL to '/results.py'
+    fetch('/results.py', {
         method: 'POST',
         body: formData,
     })
         .then(response => response.text())
         .then(data => {
-            document.getElementById('info-container').style.display = 'none'; // Hide the form
-            document.getElementById('results').style.display = 'block'; // Show the results
-            document.querySelector('#results table').innerHTML = data;
+            console.log('Response received:', data); // Add this line for debugging
+            // Rest of your code
+        })
+        .catch(error => {
+            console.error('Error:', error); // Add this line for debugging
         });
 });
