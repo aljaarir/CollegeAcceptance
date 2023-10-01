5c99aaf1d7b70e588cbf9c96e9e27dfcda47f4df
aljaarir <133610949+aljaarir@users.noreply.github.com>
2023-09-30 21:34:49 -0400

    results.py

--- results.py
+++ results.py
@@ -2,14 +2,14 @@ import cgi
 
 # Sample data for Ivy League colleges
 colleges = [
-    {"name": "Harvard University", "gpaRequirement": 3.4 " ,acceptanceRate": 3.4, "tuitionInState": 55587, "tuitionOutOfState": 55587},
-    {"name": "Yale University", "gpaRequirement": 3.4" ,acceptanceRate": 5.3, "tuitionInState": 59950, "tuitionOutOfState": 59950},
-    {"name": "Princeton University", "gpaRequirement": 3.4 " ,acceptanceRate": 5.8, "tuitionInState": 56010, "tuitionOutOfState": 56010},
-    {"name": "University of Pennsylvania", "gpaRequirement": 3.4 " ,acceptanceRate": 5.9, "tuitionInState": 61710, "tuitionOutOfState": 55000},
-    {"name": "Brown University", "gpaRequirement": 3.4 " ,acceptanceRate": 5.5, "tuitionInState": 62304, "tuitionOutOfState": 62304},
-    {"name": "Columbia University", "gpaRequirement": 3.4 " ,acceptanceRate": 4.1, "tuitionInState": 63530, "tuitionOutOfState": 55000},
-    {"name": "Cornell University", "gpaRequirement": 3.4 " , acceptanceRate": 8.7, "tuitionInState": 61015, "tuitionOutOfState": 55000},
-    {"name": "Dartmouth College", "gpaRequirement": 3.4 " , acceptanceRate": 6.2, "tuitionInState": 60870, "tuitionOutOfState": 55000}
+    {"name": "Harvard University", "gpaRequirement": 3.4, "acceptanceRate": 3.4, "tuitionInState": 55587, "tuitionOutOfState": 55587},
+    {"name": "Yale University", "gpaRequirement": 3.4, "acceptanceRate": 5.3, "tuitionInState": 59950, "tuitionOutOfState": 59950},
+    {"name": "Princeton University", "gpaRequirement": 3.4, "acceptanceRate": 5.8, "tuitionInState": 56010, "tuitionOutOfState": 56010},
+    {"name": "University of Pennsylvania", "gpaRequirement": 3.4, "acceptanceRate": 5.9, "tuitionInState": 61710, "tuitionOutOfState": 55000},
+    {"name": "Brown University", "gpaRequirement": 3.4, "acceptanceRate": 5.5, "tuitionInState": 62304, "tuitionOutOfState": 62304},
+    {"name": "Columbia University", "gpaRequirement": 3.4, "acceptanceRate": 4.1, "tuitionInState": 63530, "tuitionOutOfState": 55000},
+    {"name": "Cornell University", "gpaRequirement": 3.4, "acceptanceRate": 8.7, "tuitionInState": 61015, "tuitionOutOfState": 55000},
+    {"name": "Dartmouth College", "gpaRequirement": 3.4, "acceptanceRate": 6.2, "tuitionInState": 60870, "tuitionOutOfState": 55000}
 ]
 
 # Function to find matching colleges based on GPA
