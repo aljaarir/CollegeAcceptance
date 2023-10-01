22366633b2b0dc01ed1a27d231bcd981abece529
aljaarir <133610949+aljaarir@users.noreply.github.com>
2023-09-30 22:50:08 -0400

    results.py

--- results.py
+++ results.py
@@ -1,87 +1,14 @@
-import cgi
-
-# Sample data for Ivy League colleges
-colleges = [
-    {"name": "Harvard University", "gpaRequirement": 3.4, "acceptanceRate": 3.4, "tuitionInState": 55587, "tuitionOutOfState": 55587},
-    {"name": "Yale University", "gpaRequirement": 3.4, "acceptanceRate": 5.3, "tuitionInState": 59950, "tuitionOutOfState": 59950},
-    {"name": "Princeton University", "gpaRequirement": 3.4, "acceptanceRate": 5.8, "tuitionInState": 56010, "tuitionOutOfState": 56010},
-    {"name": "University of Pennsylvania", "gpaRequirement": 3.4, "acceptanceRate": 5.9, "tuitionInState": 61710, "tuitionOutOfState": 55000},
-    {"name": "Brown University", "gpaRequirement": 3.4, "acceptanceRate": 5.5, "tuitionInState": 62304, "tuitionOutOfState": 62304},
-    {"name": "Columbia University", "gpaRequirement": 3.4, "acceptanceRate": 4.1, "tuitionInState": 63530, "tuitionOutOfState": 55000},
-    {"name": "Cornell University", "gpaRequirement": 3.4, "acceptanceRate": 8.7, "tuitionInState": 61015, "tuitionOutOfState": 55000},
-    {"name": "Dartmouth College", "gpaRequirement": 3.4, "acceptanceRate": 6.2, "tuitionInState": 60870, "tuitionOutOfState": 55000}
-]
-
-
-
-# Get form data
-form = cgi.FieldStorage()
-name = form.getvalue("name")
-gpa = float(form.getvalue("gpa"))
-field = form.getvalue("field")
-state = form.getvalue("state")
-
-# Function to find matching colleges based on GPA
-def find_matching_colleges(gpa):
-    matching_colleges = []
-    for college in colleges:
-        if gpa >= college["gpaRequirement"]:
-            matching_colleges.append(college)
-    return matching_colleges
-
-# Find matching colleges based on GPA
-matching_colleges = find_matching_colleges(gpa)
-
-# Generate a result table in HTML
-result_html = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>College Matches</title>
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        table, th, td {
-            border: 1px solid black;
-        }
-
-        th, td {
-            padding: 10px;
-            text-align: left;
-        }
-    </style>
-</head>
-<body>
-    <h1>College Matches</h1>
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>GPA Requirement</th>
-            <th>Acceptance Rate</th>
-            <th>Tuition (In-State)</th>
-            <th>Tuition (Out-of-State)</th>
-        </tr>
-"""
-
-for college in matching_colleges:
-    result_html += f"""
-        <tr>
-            <td>{college["name"]}</td>
-            <td>{college["gpaRequirement"]}</td>
-            <td>{college["acceptanceRate"]}%</td>
-            <td>${college["tuitionInState"]}</td>
-            <td>${college["tuitionOutOfState"]}</td>
-        </tr>
-    """
-
-result_html += """
-    </table>
-</body>
-</html>
-"""
-
-print("Content-type: text/html\n")
-print(result_html)
+document.getElementById("student-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const formData = new FormData(this);
+    fetch("/results.py", { // Update the URL to '/results.py'
+        method: "POST",
+        body: formData
+    })
+    .then((response) => response.text())
+    .then((data) => {
+        document.getElementById("info-container").style.display = "none"; // Hide the form
+        document.getElementById("results").style.display = "block"; // Show the results
+        document.querySelector("#results table").innerHTML = data;
+    });
+});
